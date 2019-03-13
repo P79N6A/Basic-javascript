e7dc88ddc8483ec91e89b3895889ca2b6dfd1309
v_ljianlliu <v_ljianlliu@tencent.com>
2019-03-14 02:43:01 +0800

    tt

--- imoocDemo/imooc-manager/src/pages/ui/buttons.js
+++ imoocDemo/imooc-manager/src/pages/ui/buttons.js
@@ -1,12 +1,15 @@
 import React , { Component } from 'react';
-
-
+import { Card, Button } from 'antd';
+import './ui.less';
 export default class Buttons extends Component {
 
   render() {
     return (
       <div>
-        Button is here.
+        <Card title="基础按钮" className="card-warp">
+          <Button type="primary">按钮</Button>
+          <Button>按钮</Button>
+        </Card>
       </div>
     );
   }
